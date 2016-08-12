e651499e81cf7c03d65dd82c9a6ff31ee34eaf44
Joel Wasserman <joel.wasserman8@gmail.com>
2016-08-12 09:28:23 -0700

    Working on speed and distance label updates

--- CycleCash/CycleCash/DashboardVC.swift
+++ CycleCash/CycleCash/DashboardVC.swift
@@ -9,9 +9,9 @@
 import UIKit
 import CoreLocation
 
-class DashboardVC: UIViewController {
+class DashboardVC: UIViewController, CLLocationManagerDelegate {
 
-    @IBOutlet weak var milesLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
@@ -33,6 +33,7 @@ class DashboardVC: UIViewController {
     var startTime:NSTimeInterval?
     var speed:CLLocationSpeed = CLLocationSpeed()
     let locationManager = CLLocationManager()
+    lazy var locations = [CLLocation]()
 
     let fontOfChoice = GlobalSettings.SharedInstance.Font
     
@@ -53,7 +54,7 @@ class DashboardVC: UIViewController {
         self.stopButton.backgroundColor = UIColor.redColor()
         self.stopButton.layer.opacity = 0.5
         
-        self.milesLabel.font = UIFont(name: fontOfChoice, size: 75.0)
+        self.distanceLabel.font = UIFont(name: fontOfChoice, size: 75.0)
         self.durationLabel.font = UIFont(name: fontOfChoice, size: 40.0)
         self.speedLabel.font = UIFont(name: fontOfChoice, size: 40.0)
         self.stopButton.titleLabel?.font = UIFont(name: fontOfChoice, size: 34.0)
@@ -68,8 +69,15 @@ class DashboardVC: UIViewController {
         bottomHr.layer.shadowOpacity = 0.5
         bottomHr.layer.shadowRadius = 1.0
         
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.activityType = .Fitness
+        locationManager.requestAlwaysAuthorization()
+        
         self.resetTimer()
         self.resetDistance()
+        self.stopButton.enabled = false
+        self.stopButton.backgroundColor = UIColor.grayColor()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -82,17 +90,57 @@ class DashboardVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("locations \(locations)")
+        for location in locations {
+            if location.horizontalAccuracy < 20 {
+                //update distance
+                if self.locations.count > 0 {
+                    distance += location.distanceFromLocation(self.locations.last!)
+                }
+                
+                //save location
+                self.locations.append(location)
+            }
+        }
+        self.updateDistanceLabel()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error \(error)")
+    }
+    
     @IBAction func stopPressed(sender: AnyObject) {
         self.resetTimer()
+        self.resetDistance()
+        
+        self.startButton.enabled = true
+        self.stopButton.enabled = false
+        self.stopButton.backgroundColor = UIColor.grayColor()
+        self.startButton.backgroundColor = UIColor.greenColor()
     }
 
     @IBAction func startPressed(sender: AnyObject) {
-        duration = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(self.updateDurationLabel), userInfo: nil, repeats: true)
+        duration = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(self.updateRun), userInfo: nil, repeats: true)
         startTime = NSDate.timeIntervalSinceReferenceDate()
+        locationManager.startUpdatingLocation()
+        
+        self.distanceLabel.text = "0.0"
+        self.speedLabel.text = "0.0"
+        
+        self.startButton.enabled = false
+        self.stopButton.enabled = true
+        self.startButton.backgroundColor = UIColor.grayColor()
+        self.stopButton.backgroundColor = UIColor.redColor()
     }
     
     @IBAction func menuButtonPressed(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func updateRun() {
+        self.updateDurationLabel()
+        self.updateSpeedLabel()
     }
     
     func updateDurationLabel() {
@@ -126,8 +174,16 @@ class DashboardVC: UIViewController {
         
     }
     
+    func updateDistanceLabel() {
+        if (locations.count == 0) {
+            self.distanceLabel.text = "0.0"
+        } else {
+            
+        }
+    }
+    
     func resetTimer() {
-        self.durationLabel.text = "00:00:00"
+        self.durationLabel.text = "N/a"
         
         // invalidate timer if it's going
         if let timer = duration {
@@ -136,6 +192,10 @@ class DashboardVC: UIViewController {
     }
     
     func resetDistance() {
-        self.speedLabel.text = "\(locationManager.location!.speed)"
+        self.distanceLabel.text = "N/a"
+    }
+    
+    func resetSpeed() {
+        self.speedLabel.text = "N/a"
     }
 }
