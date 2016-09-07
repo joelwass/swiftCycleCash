5c08f88bed09a6bb3b1455ddfae876a807f5c468
Joel Wasserman <joel.wasserman8@gmail.com>
2016-09-07 16:01:26 -0700

    Added gitignore, working on transaction and deploying to eb

--- cyclecash-api/api/controllers/transaction.js
+++ cyclecash-api/api/controllers/transaction.js
@@ -12,76 +12,11 @@ module.exports = {
 
     createTransaction: function (req, res) {
 
-        var params = _.pick(req.body, 'email', 'password'),
-            user;
-
-        models.User.findOrCreate({ where: { email: req.body.email }, defaults: params })
-            .then(function (result) {
-                var created = result[1];
-                if (created) {
-                    user = result[0];
-                    return user;
-                } else {
-                    throw new MyError(helper.strings.EmailAlreadyExists);
-                }
-            })
-            .then(function (localUser) {
-                const json = { success: true,
-                    user: user.toJSON(),
-                    message: helper.strings.UserCreationSuccess,
-                };
-                return res.json(json);
-            })
-            .catch(function (err) {
-                if (err.name && err.name === 'MyError') {
-                    return res.status(400).json({ success: false, message: err.message });
-                }
-
-                if (err.errors) {
-                    if (err.errors.length > 0 && err.errors[0].message === 'email must be unique') {
-                        return res.status(400).json({
-                            success: false,
-                            message: helper.strings.EmailAlreadyExists, });
-                    } else if (err.errors.length > 0 && err.errors[0].message === 'Validation isEmail failed') {
-                        return res.status(400).json({ success: false, message: helper.strings.EnterValidEmail });
-                    } else if (err.errors.length > 0 && err.errors[0].message === 'Validation len failed') {
-                        return res.status(400).json({ success: false, message: helper.strings.PasswordInvalid });
-                    }
-                }
-
-                return res.status(500).json({ success: false, message: helper.strings.AnErrorHappened });
-            });
+        return res.status(200);
     },
 
     getAllTransactions: function (req, res) {
 
-        var user = req.user;
-        if (user == null) {
-            return res.status(400).json({ success: false, message: helper.strings.UserUpdateError });
-        }
-
-        req.user.save()
-            .then(function (result) {
-                return res.status(200).json({ success: true, user: user.toJSON() });
-            })
-            .catch(function (err) {
-                if (err.name && err.name === 'MyError') {
-                    return res.status(400).json({ success: false, message: err.message });
-                }
-
-                if (err.errors) {
-                    if (err.errors.length > 0 && err.errors[0].message === 'email must be unique') {
-                        return res.status(400).json({
-                            success: false,
-                            message: helper.strings.EmailAlreadyExists, });
-                    } else if (err.errors.length > 0 && err.errors[0].message === 'Validation isEmail failed') {
-                        return res.status(400).json({ success: false, message: helper.strings.EnterValidEmail });
-                    } else if (err.errors.length > 0 && err.errors[0].message === 'Validation len failed') {
-                        return res.status(400).json({ success: false, message: helper.strings.PasswordInvalid });
-                    }
-                }
-
-                return res.status(500).json({ success: false, message: helper.strings.AnErrorHappened });
-            });
+        return res.status(200);
     },
 }
